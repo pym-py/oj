08d5217c5ca2f3e2ce056ec878bd07c811ac6049
pym-py <1479774534@qq.com>
2023-11-10 14:14:47 +0800

    a

--- src/main/java/com/ResourceLeakExample.java
+++ src/main/java/com/ResourceLeakExample.java
@@ -5,21 +5,21 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class ResourceLeakExample {
-//    public static void main(String[] args) {
-//        BufferedReader reader = null;
-//        try {
-//            reader = new BufferedReader(new FileReader("test.txt"));
-//            String firstLine = reader.readLine();
-//            System.out.println(firstLine);
-//            // The reader is not closed in this execution path
-//            if (firstLine == null) {
-//                return;
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        // Resource leak: 'reader' is never closed on normal or exceptional execution paths
-//    }
+   public static void main(String[] args) {
+       BufferedReader reader = null;
+       try {
+           reader = new BufferedReader(new FileReader("test.txt"));
+           String firstLine = reader.readLine();
+           System.out.println(firstLine);
+           // The reader is not closed in this execution path
+           if (firstLine == null) {
+               return;
+           }
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+       // Resource leak: 'reader' is never closed on normal or exceptional execution paths
+   }
     
     
 //    public static void main(String[] args) {
@@ -33,10 +33,10 @@ public class ResourceLeakExample {
 //        // 不需要显式关闭BufferedReader，try-with-resources会自动处理
 //    }
 	
-	public static void main(String[] args) {
-		String password;
-        password = "12345"; 
-        System.out.println(password);
-    }
+	// public static void main(String[] args) {
+	// 	String password;
+    //     password = "12345"; 
+    //     System.out.println(password);
+    // }
 
 }
